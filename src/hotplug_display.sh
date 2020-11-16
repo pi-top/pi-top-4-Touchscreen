bdae096530daa39b058209377a85a6092e71a514
Jorge Capona <jcapona@gmail.com>
2020-11-16 15:57:33 -0300

    Use DISPLAY variable when using xrandr

--- src/hotplug_display.sh
+++ src/hotplug_display.sh
@@ -132,7 +132,7 @@ handle_gesture_support() {
 
 update_resolution() {
 	local display="${1}"
-	xrandr --output "${display}" --mode 1920x1080
+	env DISPLAY=:$(get_first_display) xrandr --output "${display}" --mode 1920x1080
 }
 
 unblank_display() {
@@ -150,7 +150,7 @@ handle_display_state() {
 
 	# Update display state - may not be connected!
 	for disp in "${displays[@]}"; do
-		if xrandr --query | grep -q "${disp} connected"; then
+		if env DISPLAY=:$(get_first_display) xrandr --query | grep -q "${disp} connected"; then
 			update_resolution "${disp}"
 			unblank_display
 			break
